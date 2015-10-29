d1c200b3bbd7799f8a1e9cd019cf4d769dff8299
mail2.rameshr@gmail.com <mail2.rameshr@gmail.com>
2015-10-29 13:38:44 +0530

    updated timing parameters

--- common/mapwrapper.h
+++ common/mapwrapper.h
@@ -34,24 +34,29 @@ class Map
         string min_req, max_req;
         int avg = 0, total = 0, num_requests = 0;
         int total_msec = 0;
+        int nerrors = 0;
 
         auto it = rmap.begin();
         for (; it != rmap.end(); ++it) {
             total_msec = (it->second.tv_sec * 1000) + (it->second.tv_usec/1000);
-            total += total_msec; 
-            ++num_requests;
-            if (total_msec < min_value) {
-                min_value = total_msec;
-                min_req = it->first;
-            }
-            if (total_msec > max_value) {
-                max_value = total_msec;
-                max_req = it->first;
+            if (total_msec == 0) ++nerrors;
+            else {
+                total += total_msec; 
+                ++num_requests;
+                if (total_msec < min_value) {
+                    min_value = total_msec;
+                    min_req = it->first;
+                }
+                if (total_msec > max_value) {
+                    max_value = total_msec;
+                    max_req = it->first;
+                }
             }
         }
         cout << "min req: " << min_req << " value " << min_value << " msec" << endl;
         cout << "max req: " << max_req << " value " << max_value << " msec" << endl;
         cout << "total time: " << total << " total requests: " << num_requests << endl;
+        cout << "errors: " << nerrors << endl;
         cout << "avg time: " << (total/num_requests) << endl;
     }
     private:
